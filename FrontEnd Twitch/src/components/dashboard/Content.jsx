74fd0ce32dc6a69f758a394f59e2beef2af59500
Informatica <informatica@alumno.local>
2024-04-23 16:38:18 -0600

    se agrego todo front

---
+++ FrontEnd Twitch/src/components/dashboard/Content.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import { Route, Routes } from "react-router-dom";
+import { Channels } from "../channel/Channels";
+import { Settings } from "../settings/Settings";
+
+export const Content = ({channels}) => {
+    return(
+        <div className="content-container">
+            <Routes>
+                <Route path="settings" element={<Settings/>}/>
+                <Route path="channels" element={<Channels channels={channels}/>}/>
+            </Routes>
+        </div>
+    )
+}   
